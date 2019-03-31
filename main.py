a79b9a64595ed887482dc1d56c44c955460dd3c5
ronoysarkar <ronoysarkar.mail@gmail.com>
2019-03-31 14:19:36 -0400

    Update

--- main.py
+++ main.py
@@ -8,20 +8,12 @@ import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 import pandas as pd
 
-cards = pd.read_csv("C:/Users/ronoy/OneDrive/Documents/forefront/kaggle set/card_detail.csv")
+data = pd.read_csv("C:/Users/ronoy/OneDrive/Documents/forefront/season-1819_csv.csv")
 
-print cards.head()
-ind_match     = cards.columns.get_loc("id")
-ind_league     = matches.columns.get_loc("league_id")
-ind_season     = matches.columns.get_loc("season")
-ind_stage      = matches.columns.get_loc("stage")
-# home team information
-ind_h_team     = matches.columns.get_loc("home_team_api_id")
-ind_h_player   = matches.columns.get_loc("home_player_1")
-ind_h_player_x = matches.columns.get_loc("home_player_X1")
-ind_h_player_y = matches.columns.get_loc("home_player_Y1")
-# away team information
-ind_a_team     = matches.columns.get_loc("away_team_api_id")
-ind_a_player   = matches.columns.get_loc("away_player_1")
-ind_a_player_x = matches.columns.get_loc("away_player_X1")
-ind_a_player_y = matches.columns.get_loc("away_player_Y1")
+data.head()
+
+homeWins = data['AwayTeam'] [(data['HomeTeam'] == 'Liverpool') & (data['FTR'] == 'H')] # print matches liverpool has won
+homeWins
+for i in homeWins:
+    if i != 'Brighton':
+        print i
